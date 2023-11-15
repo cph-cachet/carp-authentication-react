e762f5d45fc5ac4b996f8819ab6dd8a7751a9d8a
Daniel Jakab <jakdan99@gmail.com>
2023-11-15 16:11:27 +0100

    initial commit

---
+++ lib/components/AuthenticationProvider/AuthenticationProvider/index.tsx
@@ -0,0 +1,67 @@
+import { AxiosRequestConfig } from "axios";
+import { User } from "oidc-client-ts";
+import { createContext, useContext } from "react";
+import { env } from "../../../utils/env";
+import { AuthProvider } from "react-oidc-context";
+import { oidcConfig } from "../../../authenticator";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+interface AuthenticationContextType {
+  getUser: () => User | null,
+  getConfig: () => AxiosRequestConfig,
+}
+
+export const AuthenticationContext = createContext({} as AuthenticationContextType)
+
+export const useAuthentication = (): AuthenticationContextType => {
+  return useContext(AuthenticationContext);
+};
+
+export const AuthenticationProvider = ({ children }: Props) => {
+  const getUser = () => {
+    const localStorageKey = `oidc.user:${
+      env.VITE_KEYCLOAK_URL
+    }/realms/${env.VITE_KEYCLOAK_REALM}:${
+      env.VITE_KEYCLOAK_CLIENT_ID
+    }`;
+    const oidcStorage = localStorage.getItem(localStorageKey);
+  
+    if (!oidcStorage) {
+      return null;
+    }
+    return User.fromStorageString(oidcStorage);
+  };
+  
+  const getConfig = () => {
+    const token = getUser()?.access_token;
+    let config: AxiosRequestConfig = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    if (token) {
+      config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      };
+    }
+    return config;
+  };
+
+  return (
+    <AuthenticationContext.Provider
+      value={{
+        getUser,
+        getConfig,
+      }}>
+        <AuthProvider {...oidcConfig}>
+          {children}
+        </AuthProvider>
+    </AuthenticationContext.Provider>
+  );
+};
---
+++ lib/utils/env.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  VITE_KEYCLOAK_URL: z.string(),
+  VITE_KEYCLOAK_REALM: z.string(),
+  VITE_KEYCLOAK_CLIENT_ID: z.string(),
+  VITE_KEYCLOAK_REDIRECT_URI: z.string(),
+});
+
+export const env = envSchema.parse(import.meta.env)
